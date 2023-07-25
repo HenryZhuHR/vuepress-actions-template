cf6e6405eed585c6b688fc99cac4115c9759347a
HenryZhuHR <296506195@qq.com>
2023-07-26 00:45:05 +0800

    🛠 build: config and style

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'Vuepress Actions Template',
+  description: 'Vuepress Documentation Deployment Template by Github Pages Actions',
+  base: '/vuepress-actions-template/',
+})
